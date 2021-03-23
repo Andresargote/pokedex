404ed060cbd32ba0b0e496eab7c92608d0604ae3
Andres Argote <andresargote2019@gmail.com>
2021-03-23 17:59:54 +0100

    Modificado App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Home from "./pages/Home";
 import AppRouter from "./routers/AppRouter";
-import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import { withAuthenticator } from "@aws-amplify/ui-react";
 
 function App() {
   return (
