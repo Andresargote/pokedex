c27977ee1588410d1439357df8dcfbc424fc9787
Andres Argote <andresargote2019@gmail.com>
2021-03-23 22:39:16 +0100

    Modificado usePokemons

--- src/hooks/usePokemons.js
+++ src/hooks/usePokemons.js
@@ -3,28 +3,6 @@ import pokemons from "../data/pokedex.json";
 
 export default function usePokemons(){
     const [pokemonsData, setPokemons] = useState(pokemons);
-
-    /*const filterPokemons = (type, sort) => {
-        let staticPokemonsData = [...pokemons];
-
-    if (type === "all"){
-        if(sort === "ascending"){
-            staticPokemonsData = staticPokemonsData.sort((a, b) => a.number - b.number);
-        }else {
-            staticPokemonsData = staticPokemonsData.sort((a, b) => a.number - b.number).reverse();
-        }
-    } else {
-        staticPokemonsData = staticPokemonsData.filter((pokemon) => pokemon.type.toLowerCase() === type.toLowerCase());
-            if(sort === "ascending"){
-                staticPokemonsData = staticPokemonsData.sort((a, b) => a.number - b.number);
-            }else {
-                staticPokemonsData = staticPokemonsData.sort((a, b) => a.number - b.number).reverse();
-            }
-        } 
-
-        setPokemons(staticPokemonsData);
-    }*/
-
     let staticPokemonsData = [...pokemons];
     
     const filterPokemons = (type, sort) => {
